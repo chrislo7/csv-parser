90fe3f7e759a11e0a9563b8a75672129aca0441a
Chris <chrislo.7@live.com>
2020-12-23 14:05:18 -0500

    added updated_at column

--- init.sql
+++ init.sql
@@ -7,5 +7,6 @@ CREATE TABLE employee (
     phone_number varchar(255),
     email varchar(255),
     created_at timestamp DEFAULT NOW(),
+    updated_at timestamp DEFAULT NOW(),
     deleted_at timestamp NULL DEFAULT NULL
 );
