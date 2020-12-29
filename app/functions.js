0ae7ef717afa9be0638e2cdf07f60cab4de8b74e
Chris <chrislo.7@live.com>
2020-12-28 21:34:16 -0500

    added validations for required fields

--- app/functions.js
+++ app/functions.js
@@ -43,28 +43,61 @@ const readInput = (filepath) => {
     });
 };
 
+// helper function to validate employee IDs
+const validateEmployeeId = (id, index) => {
+    // Valid Employee IDs start with a letter and have a minimum of 6 digits after that.
+
+    // this regex is self created. note that the first letter ignores cases.
+    var regex = /[a-z](?:\d.*){6,}/i;
+    if (!regex.test(id)) {
+        console.error(`Removed invalid entry on Line ${index + 1}. Reason: Invalid Employee ID`);
+        return false;
+    }
+
+    return true;
+};
+
 // helper function to validate phone numbers
-const validatePhoneNumber = (phoneNumber) => {
-    console.log('test');
+const validatePhoneNumber = (phoneNumber, index) => {
+    // Valid Phone numbers are in the form: ^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$
+
+    // self-note: the provided regex does not check for min/max length
+    // e.g. having (647)1 as a phone number will pass.
+    // however, as this regex was provided as a requirement, no modification will be made to it.
+    var regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+    if (!regex.test(phoneNumber)) {
+        console.error(`Removed invalid entry on Line ${index + 1}. Reason: Invalid Phone Number`);
+        return false;
+    }
+
+    return true;
 };
 
 // helper function to validate emails
-const validateEmail = (email) => {
-    console.log('test');
+const validateEmail = (email, index) => {
+    // Valid emails are in the form: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
+    var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!regex.test(email)) {
+        console.error(`Removed invalid entry on Line ${index + 1}. Reason: Invalid Email`);
+        return false;
+    }
+
+    return true;
 };
 
-// helper function to validate employee IDs
-const validateEmployeeId = (id) => {
-    console.log('test');
-};
+
 
 // validate values
-const validateData = (arr) => {
-    // Valid Phone numbers are in the form: ^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$
-    // Valid emails are in the form: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
-    // Valid Employee IDs start with a letter and have a minimum of 6 digits after that.
+const validateData = (employees) => {
     // Print the line number and reason for each invalid entry.
-    console.log(arr);
+    employees.forEach((employee, index) => {
+        validateEmployeeId(employee.employeeId, index);
+        validatePhoneNumber(employee.phoneNumber, index);
+        validateEmail(employee.email, index);
+    });
+
+    // cleaned array with validated data.
+    return employees;
 };
 
 module.exports = {
