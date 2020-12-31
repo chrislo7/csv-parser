f0b2717c9982dacf2dd8d4e43d92fdeab776de01
Chris <chrislo.7@live.com>
2020-12-30 19:24:44 -0500

    removing constraint for NO_ZERO_DATE and added primary key indicator

--- init.sql
+++ init.sql
@@ -1,12 +1,14 @@
-USE employeedb
+SET sql_mode = '';
 
+USE employeedb
 CREATE TABLE employee (
-    employee_id varchar(255),
+    employee_id varchar(255) NOT NULL UNIQUE,
     first_name varchar(255),
     last_name varchar(255),
     phone_number varchar(255),
     email varchar(255),
-    created_at timestamp DEFAULT NOW(),
-    updated_at timestamp DEFAULT NOW(),
-    deleted_at timestamp NULL DEFAULT NULL
+    created_at TIMESTAMP DEFAULT '0000-00-00 00:00:00', 
+    updated_at TIMESTAMP DEFAULT now() ON UPDATE now(),
+    deleted_at timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (employee_id)
 );
